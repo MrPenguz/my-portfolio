963ae06176803b41bfdfe4e404e107cdaf02d8a3
MrPenguz <mohammadmasere@gmail.com>
2024-11-22 19:45:48 +0300

    adding the NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold p-[10px]    md:p-[20px]   ">
-        Hello world!
-      </h1>
-    </>
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-700 selection:text-white'>
+      {/* <div className="flixed top-0 -z-10 h-full w-full"></div> */}
+      <div className=" container mx-auto px-8">
+        <Navbar />
+      </div>
+
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className="relative h-screen w-screen">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(0,0,0,1))]"></div>Name
+      <div className="absolute inset-0 z-[-1] bg-[linear-gradient(to_right,#4f4f4f2e_1px,rgba(0,0,0,1)_1px),linear-gradient(to_bottom,#8080800a_1px,rgba(0,0,0,1)_1px)] bg-[size:14px_24px]"></div>
+      <App />
+    </div>
   </StrictMode>,
 )
